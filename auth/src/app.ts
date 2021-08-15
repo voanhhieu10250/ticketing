d229551808b11b375ee5317d10daa26125514a16
Vo Anh Hieu <voanhhieu10250@gmail.com>
2021-08-16 03:06:53 +0700

    update auth service, using own new npm package

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@hvticketing/common";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.set("trust proxy", true);
